5018b854251366dbd58ef7e718f83fedbd7127c6
Rodrigo Couto <r@rodg.co>
2021-10-24 20:04:54 -0300

    Initial Commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,52 @@
+<script>
+	function wave() {}
+</script>
+
+<div class="mainContainer">
+	<div class="dataContainer">
+		<div class="header">👋 Hey there!</div>
+
+		<div class="bio">
+			I am rodgco and I'm learning to develop web3 apps, that's pretty cool right? Connect your
+			Ethereum wallet and wave at me!
+		</div>
+
+		<button class="waveButton" on:click={wave}> Wave at Me </button>
+	</div>
+</div>
+
+<style>
+	.mainContainer {
+		display: flex;
+		justify-content: center;
+		width: 100%;
+		margin-top: 64px;
+	}
+
+	.dataContainer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		max-width: 600px;
+	}
+
+	.header {
+		text-align: center;
+		font-size: 32px;
+		font-weight: 600;
+	}
+
+	.bio {
+		text-align: center;
+		color: gray;
+		margin-top: 16px;
+	}
+
+	.waveButton {
+		cursor: pointer;
+		margin-top: 16px;
+		padding: 8px;
+		border: 0;
+		border-radius: 5px;
+	}
+</style>
