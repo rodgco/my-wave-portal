20e36d0fd98ff5ceb04a43d70f60f8ad93b21123
Rodrigo Couto <r@rodg.co>
2021-10-25 02:55:33 -0300

    Final site

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,5 @@
 /// <reference types="@sveltejs/kit" />
+
+interface Window {
+  ethereum: any;
+}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,156 @@
-<script>
-	function wave() {}
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { ethers, providers } from 'ethers';
+	import { abi } from '$lib/WavePortal.json';
+	import type { AbiCoder } from 'ethers/lib/utils';
+
+	let ethereum: providers.ExternalProvider;
+	let provider: providers.Web3Provider;
+	let currentAccount: AbiCoder;
+
+	let totalWaves: number;
+	let waves: any[] = [];
+
+	let value: string = '';
+
+	const contractAddress = <string>import.meta.env.VITE_CONTRACT_ADDRESS;
+
+	$: console.log('Connected', currentAccount);
+	$: console.log('Ethereum', ethereum);
+	$: console.log('Provider', provider);
+
+	onMount(async () => {
+		/*
+		 * First make sure we have access to window.ethereum
+		 */
+		try {
+			({ ethereum } = window);
+
+			if (!ethereum) {
+				console.log('Make sure you have metamask!');
+				return;
+			} else {
+				console.log('We have the ethereum object', ethereum);
+			}
+
+			provider = new ethers.providers.Web3Provider(ethereum);
+			totalWaves = await getTotalWaves();
+			waves = await getAllWaves();
+			console.log('Waves', waves);
+			/*
+			 * Check if we're authorized to access the user's wallet
+			 */
+			const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+			if (accounts.length !== 0) {
+				currentAccount = accounts[0];
+			} else {
+				console.log('No authorized account found');
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	});
+
+	/**
+	 * Implement your connectWallet method here
+	 */
+	async function connectWallet() {
+		try {
+			if (!ethereum) {
+				alert('Get MetaMask!');
+				return;
+			}
+
+			const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+			currentAccount = accounts[0];
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	async function getTotalWaves(): Promise<number> {
+		try {
+			if (ethereum) {
+				const wavePortalContract = new ethers.Contract(contractAddress, abi, provider);
+
+				let count = await wavePortalContract.getTotalWaves();
+				return count.toNumber();
+			} else {
+				console.log("Ethereum object doesn't exist!");
+			}
+		} catch (error) {
+			console.log(error);
+		}
+		return undefined;
+	}
+
+	async function getAllWaves(): Promise<any[]> {
+		try {
+			if (ethereum) {
+				const wavePortalContract = new ethers.Contract(contractAddress, abi, provider);
+
+				/**
+				 * Listen in for emitter events!
+				 */
+				wavePortalContract.on('NewWave', (waver, timestamp, message, winner, newTotal) => {
+					console.log('NewWave', waver, timestamp, message, winner, newTotal);
+
+					totalWaves = newTotal;
+
+					waves = [
+						...waves,
+						{
+							waver,
+							timestamp,
+							message,
+							winner
+						}
+					];
+				});
+				return await wavePortalContract.getAllWaves();
+			} else {
+				console.log("Ethereum object doesn't exist!");
+			}
+		} catch (error) {
+			console.log('---', error);
+		}
+		return undefined;
+	}
+
+	async function wave() {
+		try {
+			if (ethereum) {
+				const signer = provider.getSigner();
+				const wavePortalContract = new ethers.Contract(contractAddress, abi, signer);
+
+				let count = await wavePortalContract.getTotalWaves();
+				console.log('Retrieved total wave count...', count.toNumber());
+
+				/*
+				 * Execute the actual wave from your smart contract
+				 */
+				const waveTxn = await wavePortalContract.wave(value, { gasLimit: 300_000 });
+				console.log('Mining...', waveTxn.hash);
+				await waveTxn.wait();
+				console.log('Mined...', waveTxn.hash);
+				value = '';
+			} else {
+				console.log("Ethereum object doesn't exist!");
+			}
+		} catch (error) {
+			console.log('---', error.message);
+		}
+	}
+
+	function formatAddress(address: string): string {
+		return `${address.substring(0, 6)}...${address.substring(address.length - 4)}`;
+	}
+
+	function formatTimeStamp(timestamp: number): string {
+		const ts = new Date(timestamp * 1000);
+		return ts.toLocaleString();
+	}
 </script>
 
 <div class="mainContainer">
@@ -7,15 +158,68 @@
 		<div class="header">👋 Hey there!</div>
 
 		<div class="bio">
-			I am rodgco and I'm learning to develop web3 apps, that's pretty cool right? Connect your
-			Ethereum wallet and wave at me!
+			I am <a href="https://twitter.com/rodg_co">rodgco</a> and I'm learning to develop web3 apps, that's
+			pretty cool right? Connect your Ethereum wallet and wave at me!
 		</div>
 
-		<button class="waveButton" on:click={wave}> Wave at Me </button>
+		{#if !ethereum || provider?.network?.name != 'rinkeby'}
+			<div class="warning">
+				You need a wallet to use this app! Try <a href="https://metamask.io">Metamask</a>. And it
+				must be connected to the Rinkeby test network.
+			</div>
+		{:else}
+			<div class="bio">Total waves: {totalWaves}</div>
+
+			{#if currentAccount}
+				<form>
+					<input type="text" placeholder="message" bind:value />
+					<button type="submit" class="waveButton" on:click={wave}>Wave at Me</button>
+				</form>
+			{:else}
+				<button class="waveButton" on:click={connectWallet}>Connect Wallet</button>
+			{/if}
+
+			<ul id="waves">
+				{#each waves as wave}
+					<li>
+						<div class="message">{wave.message} {@html wave.winner ? '&#x1F3C6;' : ''}</div>
+						<div class="meta">{formatAddress(wave.waver)} - {formatTimeStamp(wave.timestamp)}</div>
+					</li>
+				{/each}
+			</ul>
+		{/if}
 	</div>
 </div>
 
 <style>
+	form {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		max-width: 600px;
+	}
+
+	input {
+		padding: 0.5rem 0.75rem;
+		margin-top: 1rem;
+	}
+
+	#waves {
+		list-style-type: none;
+	}
+
+	#waves li {
+		margin-bottom: 1rem;
+	}
+	#waves .message {
+		font-weight: bold;
+	}
+
+	#waves .meta {
+		font-size: 0.75rem;
+		color: #444;
+	}
+
 	.mainContainer {
 		display: flex;
 		justify-content: center;
@@ -42,8 +246,17 @@
 		margin-top: 16px;
 	}
 
+	.warning {
+		text-align: center;
+		color: #cf5d00;
+		background: #e0d392;
+		padding: 1rem;
+		margin-top: 1rem;
+		border: 2px solid #cf5d00;
+		border-radius: 0.25rem;
+	}
+
 	.waveButton {
-		cursor: pointer;
 		margin-top: 16px;
 		padding: 8px;
 		border: 0;
